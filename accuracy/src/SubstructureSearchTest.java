5b8a6af87d9a9e932cf1a4871b19994b7194a41b
jonalv <jonathan.alvarsson@gmail.com>
2011-05-21 16:56:40 +0200

    initial commit

---
+++ accuracy/src/SubstructureSearchTest.java
@@ -0,0 +1,18 @@
+import java.util.BitSet;
+import java.util.HashMap;
+import java.util.Map;
+
+
+public class SubstructureSearchTest {
+
+	// inchi / fingerprint
+	public Map<String, BitSet> fps = new HashMap<String, BitSet>();
+	
+	public SubstructureSearchTest(String fingerPrinterName, String[] files) {
+		
+	}
+	
+	public static void main(String args) {
+		
+	}
+}
